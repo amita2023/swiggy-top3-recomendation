2ed1ad15709b552d4d515a6e83307436d265419d
Amita Sahoo <amita.sahoo023@gmail.com>
2024-01-14 17:27:36 -0500

    cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help to recomend the 3 best food items on the homepage',
+    author='Amita Sahoo',
+    license='',
+)
